6cfdcdba4f9114bc9fea6ed0b00f380c68f75df3
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 22:36:15 +0900

    2_2 2개의 변수 합과 평균 구하기

---
+++ Java200Q/src/chapter2/Chapter2_2.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Chapter2_2 {
+    public static void main(String[] args) {
+        // 2개의 변수 x와 y의 합과 평균구하기
+        int x;
+        int y;
+
+        x = 63;
+        y = 18;
+        
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("합 = " + (x + y));
+        System.out.println("평균 = " + ((x + y) / 2));
+    }
+}
