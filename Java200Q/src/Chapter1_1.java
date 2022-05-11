c5a743806b7299296aa4681dd10f5bb09493d649
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 20:57:40 +0900

    chapter1_1 화면에 출력을 지시하는 프로그램

---
+++ Java200Q/src/Chapter1_1.java
@@ -0,0 +1,7 @@
+public class Chapter1_1 {
+    public static void main(String[] args) {
+        // 화면에 출력을 지시하는 프로그램
+        System.out.println("첫 Java 프로그램입니다.");
+        System.out.println("화면에 출력하고 있습니다.");
+    }
+}
