87e37c398eb03558b0005126d12637832c626a65
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 21:50:52 +0900

    세미콜론 없이 출력해보기

---
+++ Java200Q/src/Chapter1_2.java
@@ -0,0 +1,8 @@
+public class Chapter1_2 {
+    public static void main(String[] args) {
+        // 세미콜론 없이 출력
+        System.out.println("첫 Java 프로그램입니다.")
+        System.out.println("화면에 출력하고 있습니다.")
+        // 컴파일 에러가 발생해서 실행되지 않음
+    }
+}
