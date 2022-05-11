3645bd7e3163d7a4cda42f51424fc30c6e590554
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 21:58:41 +0900

    1_4 한 줄에 이름 출력하기

---
+++ Java200Q/src/Chapter1_4.java
@@ -0,0 +1,8 @@
+public class Chapter1_4 {
+    public static void main(String[] args) {
+        // 한 줄에 한글자씩 이름 출력하기
+        System.out.println("최");
+        System.out.println("진");
+        System.out.println("아");
+    }
+}
