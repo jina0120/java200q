a3a1c50fb10dd472d877296e646ba0fe0f4f023e
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 22:59:53 +0900

    2_4 3개의 int형 변수에 각각 값을 대입. 합과 평균을 구하는 프로그램

---
+++ Java200Q/src/chapter2/Chapter2_4.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class Chapter2_4 {
+    public static void main(String[] args) {
+        // 3개의 int형 변수에 각각 값을 대입. 합과 평균을 구하는 프로그램
+        int x, y, z;
+
+        x = 63;
+        y = 18;
+        z = 52;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        System.out.println("합 = " + (x + y + z));
+        System.out.println("평균 = " + (x + y + z) / 3);
+    }
+}
