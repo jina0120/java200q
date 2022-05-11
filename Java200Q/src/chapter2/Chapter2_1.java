d8a43704adf249f0e59001827ffbc38c5aba8317
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 22:22:55 +0900

    2_1 파일 이름, class 이름 수정

--- Java200Q/src/chapter2/Chapter2_1.java
+++ Java200Q/src/chapter2/Chapter2_1.java
@@ -1,6 +1,6 @@
 package chapter2;
 
-public class chapter2_1 {
+public class Chapter2_1 {
     public static void main(String[] args) {
         // 정수 82와 17의 합과 차를 구하는 프로그램
         System.out.println("82 + 17 = " + (82 + 17));
