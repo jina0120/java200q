c22dac027f9fd7007c2d1aaaf76d641dac6dea37
Jina Choi <dev.jina0120@gmail.com>
2022-05-11 22:44:42 +0900

    2_3 2_2의 프로그램에 실숫값 넣어 확인하기

--- Java200Q/src/chapter2/Chapter2_3.java
+++ Java200Q/src/chapter2/Chapter2_3.java
@@ -1,13 +1,14 @@
 package chapter2;
 
-public class Chapter2_2 {
+public class Chapter2_3 {
     public static void main(String[] args) {
-        // 2개의 변수 x와 y의 합과 평균구하기
+        // 2_2 와 동일한 프로그램에 실숫값을 대입하여 결과 확인
         int x;
         int y;
 
-        x = 63;
-        y = 18;
+        // int 변수에 실수를 넣어 에러 발생
+        x = 63.4;
+        y = 18.3;
 
         System.out.println("x = " + x);
         System.out.println("y = " + y);
