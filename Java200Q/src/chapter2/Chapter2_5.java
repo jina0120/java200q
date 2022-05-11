a8dd7e94872884697c8ab1698819806e5f12bace
Jina Choi <dev.jina0120@gmail.com>
2022-05-12 00:27:29 +0900

    2_5 키보드로 입력한 정숫값 출력. class 파일 무시 추가

---
+++ Java200Q/src/chapter2/Chapter2_5.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Chapter2_5 {
+    public static void main(String[] args) {
+        // 키보드에서 입력한 정숫값 출력
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("정숫값 : ");
+        int x = scn.nextInt();
+        System.out.println(x);
+    }
+}
