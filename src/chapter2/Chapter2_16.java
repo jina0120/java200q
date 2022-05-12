c6add8233b19bedbcc7f2a50daeb8b1fd58a7246
Jina Choi <dev.jina0120@gmail.com>
2022-05-13 00:33:49 +0900

    2_16 String 변수를 문자열로 초기화하거나 대입

---
+++ src/chapter2/Chapter2_16.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Chapter2_16 {
+    public static void main(String[] args) {
+        // String 변수를 문자열로 초기화하거나 대입
+        String s1 = "ABC";
+        String s2 = "XYZ";
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        s1 = "FBI";
+        s2 = "SNS";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
