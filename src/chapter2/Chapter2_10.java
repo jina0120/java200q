7c16e45e4453cfeb7d114406ac4678b0cf366619
Jina Choi <dev.jina0120@gmail.com>
2022-05-12 21:36:07 +0900

    2_10 구의 부피, 겉넓이 구하기(원주율 3.1416은 final 변수사용)

---
+++ src/chapter2/Chapter2_10.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Chapter2_10 {
+    public static void main(String[] args) {
+        // 구의 부피, 겉넓이 구하기(원주율 3.1416은 final 변수사용)
+        final double PI = 3.1416;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("반지름 : ");
+        double r = scn.nextDouble();
+    }
+}
