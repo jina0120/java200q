bebb0b5a88bfb602cde1f83516aa16de4efac571
Jina Choi <dev.jina0120@gmail.com>
2022-05-20 22:17:40 +0900

    3_14 두 정숫값의 차가 10이하인지 이상인지 알려주는 프로그램

---
+++ src/chapter3/Chapter3_14.java
@@ -0,0 +1,22 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class Chapter3_14 {
+    public static void main(String[] args) {
+        // 두 정숫값의 차가 10이하인 경우 "두 값의 차는 10이하입니다.", 그렇지 않으면 "두 값의 차는 11이상입니다." 출력 프로그램
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("정숫값 a : ");
+        int a = scn.nextInt();
+        System.out.println("정숫값 b : ");
+        int b = scn.nextInt();
+
+        int c = (a > b ? a-b : b-a);
+        if (c >= 10) {
+            System.out.println("두 값의 차는 11이상입니다.");
+        } else {
+            System.out.println("두 값의 차는 10이하입니다.");
+        }
+    }
+}
