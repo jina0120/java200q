57e2495cc3222ff1d755b353b1db674324b93954
Jina Choi <dev.jina0120@gmail.com>
2022-05-20 22:42:12 +0900

    3_14 리팩토링

--- src/chapter3/Chapter3_14.java
+++ src/chapter3/Chapter3_14.java
@@ -13,10 +13,6 @@ public class Chapter3_14 {
         int b = scn.nextInt();
 
         int c = (a > b ? a-b : b-a);
-        if (c >= 10) {
-            System.out.println("두 값의 차는 11이상입니다.");
-        } else {
-            System.out.println("두 값의 차는 10이하입니다.");
-        }
+        System.out.println("두 값의 차는 " + (c >= 10 ? "11이상" : "10이하") + "입니다.");
     }
 }
