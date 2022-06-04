215adec7b79641ab635149a42a7db28537c577a6
Jina Choi <dev.jina0120@gmail.com>
2022-06-05 02:22:18 +0900

    5_4 논리형 변수에 true나 flase를 대입해서 표시하는 프로그램

---
+++ src/chapter5/Chapter5_4.java
@@ -0,0 +1,12 @@
+package chapter5;
+
+public class Chapter5_4 {
+    public static void main(String[] args) {
+        // 논리형 변수에 true나 flase를 대입해서 표시하는 프로그램
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 = " + b2);
+    }
+}
