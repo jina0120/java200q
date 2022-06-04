396a64d2823a6457e6203f916c53c23115755e24
Jina Choi <dev.jina0120@gmail.com>
2022-06-05 01:49:19 +0900

    5

---
+++ src/chapter5/Chapter5_1.java
@@ -0,0 +1,10 @@
+package chapter5;
+
+public class Chapter5_1 {
+    public static void main(String[] args) {
+        // 8진수 12, 10진수 12, 16진수 12를 각각 10진수로 표시하는 프로그램
+        System.out.printf("8진수 12는 10진수로 %d", 012).println();
+        System.out.printf("10진수 12는 10진수로 %d", 12).println();
+        System.out.printf("16진수 12는 10진수로 %d", 0x12).println();
+    }
+}
